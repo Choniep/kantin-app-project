e104518ae1140a2b036703eb0e2fe508d829432a
Devdit <pradita.cleine@gmail.com>
2025-04-30 11:19:02 +0700

    chore: remove unused import

--- lib/services/canteen/diskon_service.dart
+++ lib/services/canteen/diskon_service.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:ukk_kantin/models/discount.dart';
 import 'package:ukk_kantin/models/stan/create_menu.dart';
 
