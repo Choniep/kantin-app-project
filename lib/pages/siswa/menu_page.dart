9d4f6a8bdfbc4b297670e8e67a94a8db39ce0d38
Devdit <pradita.cleine@gmail.com>
2025-05-05 05:27:49 +0700

    Refactor: safely cast numeric values from Firebase (as num?)

--- lib/pages/siswa/menu_page.dart
+++ lib/pages/siswa/menu_page.dart
@@ -54,7 +54,7 @@ class MenuPage extends StatelessWidget {
                         bottomLeft: Radius.circular(12),
                       ),
                       child: (menu.photo != null && menu.photo.isNotEmpty)
-                          ? Image.asset(
+                          ? Image.network(
                               menu.photo,
                               width: 100,
                               height: 100,
